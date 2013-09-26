e3f64299e910b4bbf660332a277ca3eeef081e3b
gleotta <gleotta@cuyum.com>
2013-09-26 11:35:48 -0400

    Esquleto servicios de Aduana

---
+++ src/main/java/bo/gob/aduanda/apps/service/AduanasService.java
@@ -0,0 +1,19 @@
+package bo.gob.aduanda.apps.service;
+
+import java.util.List;
+
+import bo.gob.aduanda.apps.exceptions.BusinessException;
+import bo.gob.aduanda.apps.model.Aduana;
+import bo.gob.aduanda.apps.model.Operador;
+
+public interface AduanasService {
+	
+	public void editarAduana(Aduana aduana) throws BusinessException;
+	
+	public void asignarOperadores(Aduana a, List<Operador> operadores);
+	
+	public List<Operador> listarOperadores(Aduana a);
+	
+	public void eliminarOperador(Aduana A, Operador o);
+	
+}
---
+++ src/main/java/bo/gob/aduanda/apps/service/impl/AduanaServiceImpl.java
@@ -0,0 +1,36 @@
+package bo.gob.aduanda.apps.service.impl;
+
+import java.util.List;
+
+import bo.gob.aduanda.apps.exceptions.BusinessException;
+import bo.gob.aduanda.apps.model.Aduana;
+import bo.gob.aduanda.apps.model.Operador;
+import bo.gob.aduanda.apps.service.AduanasService;
+
+public class AduanaServiceImpl implements AduanasService {
+
+	@Override
+	public void editarAduana(Aduana aduana) throws BusinessException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void asignarOperadores(Aduana a, List<Operador> operadores) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Operador> listarOperadores(Aduana a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminarOperador(Aduana A, Operador o) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
